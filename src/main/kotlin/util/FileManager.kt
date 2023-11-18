a9bd7e7397d8b190481c65066b4eb4676565d811
Christoph Alt <christoph_alt@posteo.de>
2023-11-18 18:21:32 +0100

    style(file manager): added docstrings to file manager functions

--- src/main/kotlin/util/FileManager.kt
+++ src/main/kotlin/util/FileManager.kt
@@ -17,29 +17,59 @@ fun readCSVAsKotlinDF(path: String): DataFrame<*> {
     return DataFrame.readCSV(path)
 }
 
+/**
+ * Stores a Kotlin dataframe as CSV file.
+ * @param df Kotlin dataframe to store
+ * @path Path where to store the CSV file
+ */
 fun storeKotlinDFAsCSV(df: DataFrame<*>, path: String) {
     df.writeCSV(path = path)
 }
 
+/**
+ * Stores a Kotlin dataframe as CSV file asynchronously.
+ * @param df Kotlin dataframe to store
+ * @path Path where to store the CSV file
+ */
 suspend fun storeKotlinDFAsCSVAsync(df: DataFrame<*>, path: String) = withContext(Dispatchers.IO) {
     storeKotlinDFAsCSV(df = df, path = path)
 }
 
+/**
+ * Reads asynchronously a CSV file and returns the data as a Kotlin dataframe.
+ * @param path Path to the CSV file
+ * @return Kotlin dataframe
+ */
 suspend fun readCSVAsKotlinDFAsync(path: String): DataFrame<*> {
     return withContext(Dispatchers.IO) {
         readCSVAsKotlinDF(path = path)
     }
 }
 
+/**
+ * Stores a target column from a Kotlin dataframe as CSV file.
+ * @param target column from a Kotlin dataframe
+ * @path Path where to store the CSV file
+ */
 fun storeTargetAsCSV(target: DataColumn<*>, path: String) {
     val df = target.toDataFrame()
     df.writeCSV(path = path)
 }
 
+/**
+ * Reads a CSV file as Smile dataframe.
+ * @param path Path to the CSV file
+ * @return Smile dataframe
+ */
 fun readCSVAsSmileDF(path: String): smile.data.DataFrame  {
     return smile.read.csv(path)
 }
 
+/**
+ * Reads asynchronously a CSV file and returns the data as a Smile dataframe.
+ * @param path Path to the CSV file
+ * @return Smile dataframe
+ */
 suspend fun readCSVAsSmileDFAsync(path: String): smile.data.DataFrame {
     return withContext(Dispatchers.IO) {
         readCSVAsSmileDF(path)
