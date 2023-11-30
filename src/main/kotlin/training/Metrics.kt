7553972c10ac6b1357bb4523aedc10b2d2dfd369
Christoph Alt <christoph_alt@posteo.de>
2023-11-30 23:47:10 +0100

    feat(metrics): implemented precision metric

--- src/main/kotlin/training/Metrics.kt
+++ src/main/kotlin/training/Metrics.kt
@@ -13,3 +13,11 @@ fun calculateAccuracy(yTrue: IntArray, yPred: IntArray) : Double {
     val accuracy = yTrue.zip(yPred).count { (a, b) -> a == b }.toDouble() / yTrue.size
     return round(value = accuracy, places = 4)
 }
+
+
+fun precision(yTrue: IntArray, yPred: IntArray) : Double {
+    require(value = yTrue.size == yPred.size) { "Arrays must have the same size" }
+    val truePositives = yTrue.zip(yPred).count { (a, b) -> a == b && a == 1 }
+    val falsePositives = yTrue.zip(yPred).count { (a, b) -> a != b && a == 0 }
+    return round(value = truePositives.toDouble() / (truePositives + falsePositives), places = 4)
+}
