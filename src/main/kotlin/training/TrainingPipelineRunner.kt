e14e7aca24886d919b3c7903b16e40cbea74c121
Christoph Alt <christoph_alt@posteo.de>
2023-12-08 18:45:53 +0100

    feat(pipeline): implemented time measure for pipeline runner

--- src/main/kotlin/training/TrainingPipelineRunner.kt
+++ src/main/kotlin/training/TrainingPipelineRunner.kt
@@ -3,6 +3,8 @@ package training
 import config.Algorithm
 import config.Config
 import config.ensembleAlgorithms
+import logging.ProjectLogger.logger
+import kotlin.time.measureTime
 
 
 /**
@@ -12,13 +14,15 @@ import config.ensembleAlgorithms
 fun trainingPipelineRunner(cfg: Config) {
 
     // TODO: Parallelize training pipelines
-
-    for (algorithm in cfg.train.algorithms) {
-        when (algorithm) {
-            in ensembleAlgorithms -> EnsembleTrainingPipeline(cfg = cfg, algorithm = algorithm)
-            Algorithm.LOGISTIC_REGRESSION -> LogisticRegressionTrainingPipeline(cfg = cfg, algorithm = algorithm)
-            Algorithm.DEEP_LEARNING_CLASSIFIER -> DeepLearningTrainingPipeline(cfg = cfg, algorithm = algorithm)
-            else -> throw IllegalArgumentException("No valid algorithm specified in config file")
-        }.execute()
+    val duration = measureTime {
+        for (algorithm in cfg.train.algorithms) {
+            when (algorithm) {
+                in ensembleAlgorithms -> EnsembleTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                Algorithm.LOGISTIC_REGRESSION -> LogisticRegressionTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                Algorithm.DEEP_LEARNING_CLASSIFIER -> DeepLearningTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                else -> throw IllegalArgumentException("No valid algorithm specified in config file")
+            }.execute()
+        }
     }
+    logger.info("Training pipelines duration: $duration")
 }
