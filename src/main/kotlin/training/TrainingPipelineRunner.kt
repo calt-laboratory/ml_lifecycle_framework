cdf8c8030952f5a45ff4c4a2a52547f1191000b5
Christoph Alt <christoph_alt@posteo.de>
2023-12-10 19:41:42 +0100

    feat(pipeline runner): enabled asynchronous training pipeline run

--- src/main/kotlin/training/TrainingPipelineRunner.kt
+++ src/main/kotlin/training/TrainingPipelineRunner.kt
@@ -3,6 +3,10 @@ package training
 import config.Algorithm
 import config.Config
 import config.ensembleAlgorithms
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import logging.ProjectLogger.logger
 import kotlin.time.measureTime
 
@@ -11,18 +15,19 @@ import kotlin.time.measureTime
  * Runs one or more training pipelines based on the algorithms list specified in the config file.
  * @param cfg Configurations
  */
-fun trainingPipelineRunner(cfg: Config) {
+fun trainingPipelineRunner(cfg: Config) = runBlocking {
 
-    // TODO: Parallelize training pipelines
     val duration = measureTime {
-        for (algorithm in cfg.train.algorithms) {
-            when (algorithm) {
-                in ensembleAlgorithms -> EnsembleTrainingPipeline(cfg = cfg, algorithm = algorithm)
-                Algorithm.LOGISTIC_REGRESSION -> LogisticRegressionTrainingPipeline(cfg = cfg, algorithm = algorithm)
-                Algorithm.DEEP_LEARNING_CLASSIFIER -> DeepLearningTrainingPipeline(cfg = cfg, algorithm = algorithm)
-                else -> throw IllegalArgumentException("No valid algorithm specified in config file")
-            }.execute()
-        }
+        cfg.train.algorithms.map { algorithm ->
+            async(Dispatchers.Default) {
+                when (algorithm) {
+                    in ensembleAlgorithms -> EnsembleTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                    Algorithm.LOGISTIC_REGRESSION -> LogisticRegressionTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                    Algorithm.DEEP_LEARNING_CLASSIFIER -> DeepLearningTrainingPipeline(cfg = cfg, algorithm = algorithm)
+                    else -> throw IllegalArgumentException("No valid algorithm specified in config file")
+                }.execute()
+            }
+        }.awaitAll()
     }
     logger.info("Training pipelines duration: $duration")
 }
