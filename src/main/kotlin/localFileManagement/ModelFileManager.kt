1fe67836415f3e9f4081e64b9fd6e35714bca1b9
calt-lab <christoph_alt@posteo.de>
2025-02-16 15:02:04 +0100

    feat(deep learning pipeline): added storeSmileClassifierModel()

--- src/main/kotlin/localFileManagement/ModelFileManager.kt
+++ src/main/kotlin/localFileManagement/ModelFileManager.kt
@@ -2,10 +2,15 @@ package localFileManagement
 
 import constants.PATH_TO_TRAINED_MODELS
 import datetime.createTimeStamp
+import logging.ProjectLogger.logger
 import org.jetbrains.kotlinx.dl.api.core.SavingFormat
 import org.jetbrains.kotlinx.dl.api.core.Sequential
 import org.jetbrains.kotlinx.dl.api.core.WritingMode
+import training.EnsembleClassifier
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.ObjectOutputStream
 
 
 fun saveDLClassifierModel(model: Sequential) {
@@ -16,4 +21,18 @@ fun saveDLClassifierModel(model: Sequential) {
         savingFormat = SavingFormat.TF_GRAPH_CUSTOM_VARIABLES,
         writingMode = WritingMode.OVERRIDE,
     )
+}
+
+
+fun storeSmileClassifierModel(model: EnsembleClassifier, path: File) {
+    try {
+        FileOutputStream(path).use { fileOut ->
+            ObjectOutputStream(fileOut).use { out ->
+                out.writeObject(model)
+            }
+        }
+        logger.info("Serialized data is saved in decision_tree_model.ser")
+    } catch (i: IOException) {
+        i.printStackTrace()
+    }
 }
